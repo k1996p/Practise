2ad8b7ef36e2147a2b8fdb895277acaf7c3d24e1
Krishna Prasad <84434175+k1996p@users.noreply.github.com>
2021-10-08 08:38:55 +0530

    Delete DOM Manipulation directory

--- DOM Manipulation/jqueryAddHobbies/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('button').click(function () {
-
-        var addHobbyToList = $('#input').val();
-        $('#list').append('<li>' + addHobbyToList + '</li>');
-        return false;
-    });
-});
